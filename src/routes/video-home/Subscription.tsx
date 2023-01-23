8652471f8f3c2a17fbcf84891984a658a2893333
artemkolodko <temakolodko@gmail.com>
2023-01-23 16:43:14 +0000

    Use Wallet Connect provider to send transaction

--- src/routes/video-home/Subscription.tsx
+++ src/routes/video-home/Subscription.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import {Box, Button, Text} from 'grommet';
+import React, { useState, useEffect } from 'react'
+import {Box, Button, Spinner, Text} from 'grommet';
 import Web3 from 'web3'
 import config from "../../config";
 import Countdown from "react-countdown";
@@ -17,30 +17,31 @@ export interface SubscriptionProps {
 export const Subscription = (props: SubscriptionProps) => {
     const { isSubscribed, subscriptionStart, subscriptionTimeout, onSubscribe, onUnsubscribe} = props
 
-    const { isConnected, address } = useAccount()
+    const { isConnected, address, connector } = useAccount()
     const [txHash, setTxHash] = useState('')
+    const [isConfirming, setIsConfirming] = useState(false)
 
     const onSendClicked = async () => {
         try {
-            const web3 = new Web3('https://api.harmony.one')
+            const provider = await connector!.getProvider()
+            const web3 = new Web3(provider)
             const amount = web3.utils.toWei('10', 'ether');
             const transactionParameters = {
-                nonce: '0x00',
+                // nonce: '0x00',
                 gas: web3.utils.toHex(21000),
                 gasPrice: web3.utils.toHex(1000 * 1000 * 1000 * 1000),
-                to: '0x0000000000000000000000000000000000000000',
+                to: '0x95D02e967Dd2D2B1839347e0B84E59136b11A073',
                 from: address,
                 value: web3.utils.toHex(amount),
-                chainId: window.ethereum.networkVersion
+                // chainId: window.ethereum.networkVersion
             };
-            const hash = await window.ethereum.request({
-                method: 'eth_sendTransaction',
-                params: [transactionParameters],
-            });
-            setTxHash(hash)
+            setIsConfirming(true)
+            await web3.eth.sendTransaction(transactionParameters)
             onSubscribe()
         } catch (e) {
             console.log('Cannot send tx:', e)
+        } finally {
+            setIsConfirming(false)
         }
     }
 
@@ -65,12 +66,17 @@ export const Subscription = (props: SubscriptionProps) => {
                     </Box>
                   }
                   {(isConnected && address) &&
-                    <Box>
+                    <Box direction={'row'} gap={'medium'}>
                         <Box>
-                            <Button primary onClick={onSendClicked}>
-                                Subscribe (10 ONE)
+                            <Button primary disabled={isConfirming} onClick={onSendClicked}>
+                                {isConfirming ? 'Confirming transaction...' : 'Subscribe (10 ONE)'}
                             </Button>
                         </Box>
+                        {isConfirming &&
+                          <Box>
+                              <Spinner size={'small'} />
+                          </Box>
+                        }
                     </Box>
                   }
               </Box>
